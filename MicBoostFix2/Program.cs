01ecc26ba5aa6991b97ea06e65f3b60929666b2a
HarryCMather <harrycmather@gmail.com>
2023-02-11 22:40:15 +0000

    chore: Abstract logic, use JsonSerializer

--- MicBoostFix2/Program.cs
+++ MicBoostFix2/Program.cs
@@ -1,172 +1,56 @@
 ﻿using System;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
-using Windows.Devices.Enumeration;
 using Windows.Media.Capture;
 using Windows.Media.Devices;
-using Windows.Storage;
+using MicBoostFix2.ConsoleWindowManager;
+using MicBoostFix2.SettingManagement;
 
-namespace MicBoostFix2
+namespace MicBoostFix2;
+
+class Program
 {
-    class Program
+    private static async Task Main()
     {
-        static async Task Main(string[] args)
+        Settings settings = await SettingsReader.ReadSettings() ?? await SettingsWriter.SetupInitialOptions();
+
+        try
         {
-            Settings settings = await GetSettings() ?? await SetupInitialOptions();
-
-            try
-            {
-                AudioDeviceController audioController = await GetAudioDeviceController(settings);
-                HideConsoleWindow();
-                LevelCheckerLoop(audioController, settings);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred:  This likely means you have not granted the application permission to access your microphone in Windows Settings -> Privacy -> Microphone" +
-                                  $"{ex.Message}\n");
-                Console.ReadKey(); 
-            }
+            AudioDeviceController audioController = await GetAudioDeviceController(settings.MicrophoneId);
+            ConsoleWindowHider.DisplayConsoleWindow(false);
+            await LevelCheckerLoop(audioController, settings);
         }
-
-        private static void LevelCheckerLoop(AudioDeviceController audioController, Settings settings)
+        catch (Exception ex)
         {
-            while (true)
-            {
-                if (audioController.VolumePercent > settings.MicrophoneLevel)
-                {
-                    audioController.VolumePercent = settings.MicrophoneLevel;
-                }
-                Thread.Sleep(1000);
-            }
+            ConsoleWindowHider.DisplayConsoleWindow(true);
+            Console.WriteLine("An error occurred:  This likely means you have not granted the application permission to access your microphone in Windows Settings -> Privacy -> Microphone" +
+                              $"{ex.Message}\n");
+            Console.ReadKey(); 
         }
+    }
 
-        private static async Task<string> GetMicrophoneId()
+    private static async Task LevelCheckerLoop(AudioDeviceController audioController, Settings settings)
+    {
+        while (true)
         {
-            DeviceInformationCollection microphones = await DeviceInformation.FindAllAsync(DeviceClass.AudioCapture);
-            Console.WriteLine("Enter the number corresponding to the microphone you wish to control:");
-            for (int count = 0; count < microphones.Count; count++)
+            if (audioController.VolumePercent > settings.MicrophoneLevel)
             {
-                Console.WriteLine($"{count + 1} - {microphones[count].Name}");
+                audioController.VolumePercent = settings.MicrophoneLevel;
             }
-
-            if (int.TryParse(Console.ReadLine(), out int selectedMicrophoneIndex) && ((selectedMicrophoneIndex - 1) >= 0 && selectedMicrophoneIndex - 1 < microphones.Count))
-            {
-                return microphones[selectedMicrophoneIndex - 1].Id;
-            }
-
-            return null;
+            await Task.Delay(1000);
         }
+    }
+    
+    private static async Task<AudioDeviceController> GetAudioDeviceController(string microphoneId)
+    {
+        MediaCapture mediaCapture = new MediaCapture();
 
-        private static async Task<bool> CheckValidMicrophoneId(string inputId)
+        await mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings
         {
-            string[] microphoneIds = (await DeviceInformation.FindAllAsync(DeviceClass.AudioCapture)).Select(microphone => microphone.Id).ToArray();
-            return microphoneIds.Contains(inputId);
-        }
+            MediaCategory = MediaCategory.Speech,
+            StreamingCaptureMode = StreamingCaptureMode.Audio,
+            AudioDeviceId = microphoneId
+        });
 
-        private static async Task<Settings> SetupInitialOptions()
-        {
-            string microphoneId = await GetMicrophoneId();
-
-            if (microphoneId is null)
-            {
-                ExitProgram("id");
-            }
-
-            Console.WriteLine("Enter the microphone level you wish to set in %:");
-            if (!float.TryParse(Console.ReadLine(), out float microphoneLevel))
-            {
-                ExitProgram("level");
-            }
-            else
-            {
-                if (microphoneLevel < 0.0 || microphoneLevel > 100.0)
-                {
-                    ExitProgram("level");
-                }
-            }
-
-            Settings settings = new Settings
-            {
-                MicrophoneId = microphoneId,
-                MicrophoneLevel = Convert.ToSingle(Math.Round(Convert.ToDouble(microphoneLevel), 0, MidpointRounding.AwayFromZero))
-            };
-
-            StorageFolder folder = ApplicationData.Current.LocalFolder;
-            StorageFile file = await folder.CreateFileAsync("MicSettings.txt");
-            await FileIO.WriteTextAsync(file, $"{settings.MicrophoneId}\n{settings.MicrophoneLevel}");
-
-            return settings;
-        }
-
-        private static void ExitProgram(string messageVariation)
-        {
-            Console.WriteLine($"You did not enter a valid microphone {messageVariation}, the program will exit in 5 seconds...");
-            Thread.Sleep(5000);
-            Environment.Exit(0);
-        }
-
-        private static async Task<Settings> GetSettings()
-        {
-            if (await ApplicationData.Current.LocalFolder.TryGetItemAsync("MicSettings.txt") == null)
-            {
-                return null;
-            }
-
-            StorageFolder folder = ApplicationData.Current.LocalFolder;
-            StorageFile file = await folder.GetFileAsync("MicSettings.txt");
-            string[] contents = (await FileIO.ReadTextAsync(file)).Split('\n');
-
-            if (!float.TryParse(contents[1], out float microphoneLevel))
-            {
-                return null;
-            }
-
-            if (microphoneLevel < 0.0 || microphoneLevel > 100.0)
-            {
-                ExitProgram("level");
-            }
-
-            if (!await CheckValidMicrophoneId(contents[0]))
-            {
-                ExitProgram("ID in the file - this was likely manually modified");
-            }
-
-            return new Settings
-            {
-                MicrophoneId = contents[0],
-                MicrophoneLevel = microphoneLevel
-            };
-        }
-
-        private static async Task<AudioDeviceController> GetAudioDeviceController(Settings settings)
-        {
-            MediaCapture mediaCapture = new MediaCapture();
-
-            await mediaCapture.InitializeAsync(new MediaCaptureInitializationSettings
-            {
-                MediaCategory = MediaCategory.Speech,
-                StreamingCaptureMode = StreamingCaptureMode.Audio,
-                AudioDeviceId = settings.MicrophoneId
-            });
-
-            return mediaCapture.AudioDeviceController;
-        }
-
-        private static void HideConsoleWindow()
-        {
-            IntPtr consoleWindowHandle = GetConsoleWindow();
-            ShowWindow(consoleWindowHandle, SW_HIDE);
-        }
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        private const int SW_HIDE = 0;
-        private const int SW_SHOW = 5;  //not used
+        return mediaCapture.AudioDeviceController;
     }
 }
---
+++ MicBoostFix2/SettingManagement/SettingsReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Storage;
+using MicBoostFix2.ConsoleWindowManager;
+using Newtonsoft.Json;
+
+namespace MicBoostFix2.SettingManagement;
+
+public static class SettingsReader
+{
+    public static async Task<Settings> ReadSettings()
+    {
+        if (await ApplicationData.Current.LocalFolder.TryGetItemAsync("MicSettings.txt") is null)
+        {
+            return null;
+        }
+
+        StorageFolder folder = ApplicationData.Current.LocalFolder;
+        StorageFile file = await folder.GetFileAsync("MicSettings.txt");
+        string settingsJson = await FileIO.ReadTextAsync(file);
+
+        Settings settings = JsonConvert.DeserializeObject<Settings>(settingsJson);
+        await ValidateSettings(settings);
+        
+        return settings;
+    }
+    
+    private static async Task ValidateSettings(Settings settings)
+    {
+        if (settings.MicrophoneLevel is < 0.0f or > 100.0f)
+        {
+            ConsoleWindowHider.DisplayConsoleWindow(true);
+            throw new ArgumentOutOfRangeException(nameof(settings.MicrophoneLevel), "Error: Microphone level was invalid. Exiting program.");
+        }
+
+        if (!await CheckValidMicrophoneId(settings.MicrophoneId))
+        {
+            ConsoleWindowHider.DisplayConsoleWindow(true);
+            throw new ArgumentOutOfRangeException(nameof(settings.MicrophoneId), "Error: An invalid Microphone ID was specified. Exiting program.");
+        }
+    }
+
+    private static async Task<bool> CheckValidMicrophoneId(string microphoneId)
+    {
+        return (await DeviceInformation.FindAllAsync(DeviceClass.AudioCapture))
+            .Select(microphone => microphone.Id)
+            .Contains(microphoneId);
+    }
+}
